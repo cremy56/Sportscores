43f776dad6f40fb7e6674e2671989c13736e900c
Lemmy7 <cremy56@hotmail.com>
2025-07-08 14:14:19 +0200

    Mijn update

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -1,6 +1,9 @@
-import { createClient } from '@supabase/supabase-js'
+// src/supabaseClient.js
+import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = 'https://pblywyrfsrrpnwbjdxoh.supabase.co'
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InBibHl3eXJmc3JycG53YmpkeG9oIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTEzNjUwMzQsImV4cCI6MjA2Njk0MTAzNH0.VLXPDpCybUhK9fRVEFm0vDM4S4_-of07TYHekFQVXi0'
+const supabase = createClient(
+  import.meta.env.VITE_SUPABASE_URL,
+  import.meta.env.VITE_SUPABASE_ANON_KEY
+);
 
-export const supabase = createClient(supabaseUrl, supabaseKey)
+export { supabase };
