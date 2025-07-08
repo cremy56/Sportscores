3fe862b95c76ff137c731de3e7c4ebd5e269c1f8
Lemmy7 <cremy56@hotmail.com>
2025-07-08 15:28:44 +0200

    Mijn update

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -115,13 +115,13 @@ export default function Layout() {
           </ul>
 
           {/* Mobiele navigatie dropdown */}
-          <ul
+         <ul
+  style={{ backgroundColor: 'white' }}
   className={`md:hidden absolute top-full left-0 right-0 border border-gray-200 rounded-b-md py-4 px-6 flex flex-col space-y-3 transition-transform duration-300 ease-in-out
     ${mobileMenuOpen ? 'translate-y-0 opacity-100 pointer-events-auto' : '-translate-y-10 opacity-0 pointer-events-none'}
-    bg-white dark:bg-white
   `}
-            onClick={() => setMobileMenuOpen(false)}
-          >
+  onClick={() => setMobileMenuOpen(false)}
+>
             <li>
               <NavLink to="/" className={({ isActive }) => (isActive ? activeLinkStyle : inactiveLinkStyle)}>
                 Highscores
