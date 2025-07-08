c2278994580ad304dbe2c4045ae4ac00286379b2
Lemmy7 <cremy56@hotmail.com>
2025-07-08 16:58:20 +0200

    Mijn update

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -116,7 +116,7 @@ export default function Layout() {
 
           {/* Mobiele navigatie dropdown */}
         <ul
-          className={`mobile-menu md:hidden absolute top-full left-0 right-0 border border-gray-200 rounded-b-md py-4 px-6 flex flex-col space-y-3 transition-transform duration-300 ease-in-out
+          className={`mobile-menu bg-white text-black md:hidden absolute top-full left-0 right-0 border border-gray-200 rounded-b-md py-4 px-6 flex flex-col space-y-3 transition-transform duration-300 ease-in-out
            ${mobileMenuOpen ? 'translate-y-0 opacity-100 pointer-events-auto' : '-translate-y-10 opacity-0 pointer-events-none'}
           `}
             onClick={() => setMobileMenuOpen(false)}
