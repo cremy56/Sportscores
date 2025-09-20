ac28f42bfba6c3f8aabd67c6d60cbfa16a76f5db
Lemmy7 <cremy56@hotmail.com>
2025-09-20 22:54:58 +0200

    Beschrijvende bericht

--- src/utils/smartschoolAuth.js
+++ src/utils/smartschoolAuth.js
@@ -1,7 +1,7 @@
 // src/utils/smartschoolAuth.js
 const SMARTSCHOOL_CONFIG = {
   clientId: 'abc833209402',
-  redirectUri: 'Https://www.sportscores.be/auth/smartschool/callback', // Exacte productie URL
+  redirectUri: 'https://www.sportscores.be/auth/smartschool/callback?sp-oauth=smartschool',
   scope: 'userinfo fulluserinfo'
 };
 
