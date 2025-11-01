090febdc69e25b9e9dfa263e318a3ceb48c42bdd
Lemmy7 <cremy56@hotmail.com>
2025-11-01 20:52:27 +0100

    Beschrijvende bericht

--- api/getUsers.js
+++ api/getUsers.js
@@ -17,15 +17,7 @@ const decryptName = (encryptedName, masterKey) => {
 };
 
 export default async function handler(req, res) {
-    res.setHeader('Access-Control-Allow-Credentials', true);
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Methods', 'POST, OPTIONS');
-    res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
-
-    if (req.method === 'OPTIONS') {
-        res.status(200).end();
-        return;
-    }
+    
 
     if (req.method !== 'POST') {
         return res.status(405).json({ error: 'Method Not Allowed' });
