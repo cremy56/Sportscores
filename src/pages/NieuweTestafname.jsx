71cbaf9e2e6a48f9cd4246d90e2e9f4466b8947c
Lemmy7 <cremy56@hotmail.com>
2026-04-25 10:44:42 +0200

    Fix: verwijder foutieve console.log buiten component

--- src/pages/NieuweTestafname.jsx
+++ src/pages/NieuweTestafname.jsx
@@ -117,7 +117,6 @@ function formatTimeAgo(pastDate, referenceDate) {
 }
 
 // --- HOOFDCOMPONENT ---
-console.log("NieuweTestafname render start", { token: profile?._token ? "aanwezig" : "ONTBREEKT", school_id: profile?.school_id });
 export default function NieuweTestafname() {
     const { profile } = useOutletContext();
     const [groepen, setGroepen] = useState([]);
