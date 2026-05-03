5b0fc4202c776ec04cb0a892ba6940f4abd55b1e
Lemmy7 <cremy56@hotmail.com>
2026-05-03 11:27:41 +0200

    fix(sport-lab): ontbrekende PlayIcon en StopIcon imports

--- src/pages/SportLab.jsx
+++ src/pages/SportLab.jsx
@@ -8,6 +8,8 @@ import {
     CheckCircleIcon,
     ClockIcon,
     ShieldExclamationIcon,
+    PlayIcon,
+    StopIcon,
 } from '@heroicons/react/24/outline';
 import { CheckCircleIcon as CheckCircleSolid } from '@heroicons/react/24/solid';
 
